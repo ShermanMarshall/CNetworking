f43d5ffcc3a3fbb5b5c03eff958cacb9b4873608
sherman marshall <sherman.david.marshall@gmail.com>
2015-07-20 00:16:53 -0700

    Update. Synching before bedtime
    Includes the header file, and all bugs seem isolated to convertIP.
    Running some tests to determine where the issue lies.

--- simpleserver.h
+++ simpleserver.h
@@ -7,12 +7,14 @@
 #include "stdio.h"
 #include "stdlib.h"
 #include "math.h"
+#include "string.h"
 
 //Constants
 #define TMP_BYTES 3
+#define QUAD 4
 
 //Prototypes
 int convertIP (char* quad);
-char** getQuad(char* quad);
+char** getQuad(char* quad, int* err);
 
 #endif
